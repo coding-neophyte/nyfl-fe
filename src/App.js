28fb5347093deb79e462911ff458e2b689018e37
Julius Franklin <jjfranklin88@outlook.com>
2022-04-13 17:46:48 -0400

    fixing ci issues

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Signup from '../src/Views/Signup';
 import Confirmation from './Views/Confirmation';
 
